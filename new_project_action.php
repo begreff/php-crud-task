3bb225ac4b81581b960f078ba4871de027f83da6
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-20 16:55:28 +0000

    Automatically create and assign groups when creating a project

--- new_project_action.php
+++ new_project_action.php
@@ -3,13 +3,26 @@
 require 'templates/bootstrap.php';
 
 use src\objects\Project;
+use src\objects\Group;
+
 $projectGateway = new Project($db->getConnection());
+$groupGateway = new Group($db->getConnection());
 
-
-$projectGateway->create([
+// create project
+$id = $projectGateway->create([
     'title' => $_POST['titleInput'],
     'num_groups' => $_POST['numOfGroupsInput'],
     'students_per_group' => $_POST['studentsPerGroupInput']
 ]);
+
+// create and assign groups to project
+if ($id) {
+    for ($i = 1; $i <= $_POST['numOfGroupsInput']; $i++) {
+        $groupGateway->create([
+            'project_id' => $id,
+            'group_number' => $i,
+        ]);
+    }
+}
 
 header('Location: index.php');
