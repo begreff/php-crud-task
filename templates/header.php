b8a1de45dbf15a9ea2e299d173bf18052162a4ee
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-20 12:08:30 +0000

    Move header to templates folder

--- templates/header.php
+++ templates/header.php
@@ -20,38 +20,3 @@
     </button>
 </nav>
 <div class="container">
-
-    <?php
-
-    require '../vendor/autoload.php';
-    use config\Database;
-    use src\objects\Project;
-
-    $config = require '../config/config.php';
-    $db = new Database($config['database']);
-
-    $projectGateway = new Project($db->getConnection());
-    $projects = $projectGateway->all();
-
-    ?>
-
-    <h1 class="my-3">All Projects</h1>
-
-    <?php if ($projects) : ?>
-        <?php foreach ($projects as $project) :?>
-            <div class="card my-3">
-                <div class="card-body">
-                    <li><a href="project_view.php?id=<?= $project['id']?>"> <?= $project['title'] ?> </a></li>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <p>There are no projects yet.</p>
-    <?php endif ?>
-
-    <button type="button" class="btn btn-primary"><a href="new_project.php" class="text-light">Add New Project</a></button>
-
-</div>
-<footer class="my-4"></footer>
-</body>
-</html>
