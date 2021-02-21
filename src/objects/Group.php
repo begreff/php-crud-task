20e63c18eb7f29f1c0bcc8291b77a40a1285bbfd
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-21 10:07:48 +0000

    Add create and all methods

--- src/objects/Group.php
+++ src/objects/Group.php
@@ -17,32 +17,6 @@ class Group
         $this->db_conn = $db_conn;
     }
 
-    public function all($project_id)
-    {
-        $sql = "SELECT * FROM student_groups WHERE project_id = ?";
-
-        try {
-            $stmt = $this->db_conn->prepare($sql);
-            $stmt->execute(array($project_id));
-            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            exit($e->getMessage());
-        }
-    }
-
-    public function read($project_id, $group_number)
-    {
-        $sql = "SELECT * FROM student_groups WHERE project_id = ? AND group_number = ?";
-
-        try {
-            $stmt = $this->db_conn->prepare($sql);
-            $stmt->execute(array($project_id, $group_number));
-            return $stmt->fetch(\PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            exit($e->getMessage());
-        }
-    }
-
     public function create($parameters)
     {
         $sql = "INSERT INTO student_groups (project_id, group_number)
@@ -54,10 +28,50 @@ class Group
                 'group_number' => $parameters['group_number'],
             ));
         } catch (\Exception $e) {
-            print_r($e->getMessage());
+            exit($e->getMessage());
         }
     }
 
+    public function all($project_id)
+    {
+        $data = [];
+        $groups = $this->getAllGroups($project_id);
+        foreach ($groups as $group) {
+            $groupMembers = $this->getGroupMembers($project_id, $group['group_number']);
+            $data[$group['group_number']] = $groupMembers;
+        }
+        return $data;
+    }
 
+    private function getAllGroups($project_id): array
+    {
+        $sql = "SELECT project_id, group_number  
+                FROM student_groups
+                WHERE project_id = ? ";
 
+        try {
+            $stmt = $this->db_conn->prepare($sql);
+            $stmt->execute(array($project_id));
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    private function getGroupMembers($projectID, $groupNumber): array
+    {
+        $sql = "SELECT student_groups.group_number, students.firstname, students.lastname  
+                FROM student_groups
+                INNER JOIN students 
+                    ON student_groups.project_id = students.project_id AND student_groups.group_number = students.group_number
+                WHERE student_groups.project_id = ? AND student_groups.group_number = ? ";
+
+        try {
+            $stmt = $this->db_conn->prepare($sql);
+            $stmt->execute(array($projectID, $groupNumber));
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
 }
