f397a46e0673b44aa6d35eff744e4888788a685c
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-22 15:02:06 +0000

    Change header location

--- update_group_action.php
+++ update_group_action.php
@@ -8,9 +8,8 @@ use src\StudentRepository;
 $studentGateway = new StudentRepository($db->getConnection());
 
 $student_id = (int)$_POST['studentSelected'];
-$project_id = (int)$_GET['project_id'];
 $group_number= (int)$_GET['group_number'];
 
 $studentGateway->update($student_id, $group_number);
 
-header("Location: project_view.php?id=" . $_GET['project_id']);
+header('Location: '.$_SERVER['HTTP_REFERER']);
