03887dc201c9bd65a54fc9d1562b43773ed1a72d
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-23 15:04:35 +0000

    Add helper functions to group repository

--- src/GroupRepository.php
+++ src/GroupRepository.php
@@ -25,4 +25,51 @@ class GroupRepository
             exit($e->getMessage());
         }
     }
+
+    public function all($projectID, $groupNumber)
+    {
+        $sql = "SELECT id, firstname, lastname, group_number 
+            FROM students
+            WHERE project_id = ? AND group_number = ?
+            ORDER BY id";
+
+        try {
+            $stmt = $this->db_conn->prepare($sql);
+            $stmt->execute(array($projectID, $groupNumber));
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function unassigned($projectID)
+    {
+        $sql = "SELECT id, firstname, lastname, group_number 
+            FROM students
+            WHERE project_id = ? AND group_number IS NULL
+            ORDER BY id";
+
+        try {
+            $stmt = $this->db_conn->prepare($sql);
+            $stmt->execute(array($projectID));
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function projectInfo($projectID)
+    {
+        $sql = "SELECT id, num_groups AS total, students_per_group AS capacity
+                FROM projects
+                WHERE id = ?";
+
+        try {
+            $stmt = $this->db_conn->prepare($sql);
+            $stmt->execute(array($projectID));
+            return $stmt->fetch(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
 }
