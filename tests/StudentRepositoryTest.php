fcbc8c5e512f3115d56b568c0c576bdc5e53677b
Tomas Mikus <tomas.mikus@outlook.com>
2021-02-25 12:22:40 +0000

    Add a test for deleting a student

--- tests/StudentRepositoryTest.php
+++ tests/StudentRepositoryTest.php
@@ -8,19 +8,24 @@ use src\models\StudentRepository;
 
 class StudentRepositoryTest extends TestCase
 {
-
     use TestCaseTrait;
     private $studentRepo;
 
-    public function setUp()
-    {
-        $this->studentRepo = new StudentRepository($this->getConnection());
-    }
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
 
-    public function getConnection()
-    {
-        $pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
-        return $this->createDefaultDBConnection($pdo, $GLOBALS['DB_DBNAME']);
+    // only instantiate PHPUnit\DbUnit\Database\Connection once per test
+    private $conn = null;
+
+    final public function getConnection() {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new \PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        }
+
+        return $this->conn;
     }
 
     public function getDataSet()
@@ -30,7 +35,24 @@ class StudentRepositoryTest extends TestCase
 
     public function testRowCount()
     {
-        $this->assertSame(2, $this->getConnection()->getRowCount('students'), "Pre-condition");
+        $this->assertSame(3, $this->getConnection()->getRowCount('students'), "Initial size");
+    }
+
+    public function testStudentDelete()
+    {
+        // Create student repo
+        $this->studentRepo = new StudentRepository($this->getConnection()->getConnection());
+
+        // Delete student
+        $this->studentRepo->delete(3);
+
+        // Assert row count is as expected (from 3 to 2)
+        $this->assertSame(2, $this->getConnection()->getRowCount('students'), 'After delete');
+
+        // Assert correct data was deleted
+        $tableAfterDelete = $this->getConnection()->createQueryTable('after delete', 'SELECT * FROM students');
+        $expectedTable = $this->createFlatXMLDataSet(dirname(__FILE__).'/students_expected.xml')->getTable('students');
+        $tableAfterDelete->matches($expectedTable);
     }
 }
 
